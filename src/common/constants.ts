d3d50bd11d03d78e1e448d4661c26c1e31a28408
JoaoCampos89 <jcampos893@gmail.com>
2019-11-08 11:42:23 -0300

    change constant to update markets

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -64,8 +64,8 @@ export const UPDATE_TOKENS_PRICE_INTERVAL: number = process.env.REACT_APP_UPDATE
 
 // Default value is enabled, 0 is disabled
 export const UPDATE_ERC20_MARKETS: number = process.env.REACT_APP_UPDATE_ERC20_MARKETS_INTERVAL
-    ? Number.parseInt(process.env.REACT_APP_UPDATE_ETHER_PRICE_INTERVAL as string, 10)
-    : 3600000;
+    ? Number.parseInt(process.env.REACT_APP_UPDATE_ERC20_MARKETS_INTERVAL as string, 10)
+    : 60000;
 
 export const NOTIFICATIONS_LIMIT: number =
     Number.parseInt(process.env.REACT_APP_NOTIFICATIONS_LIMIT as string, 10) || 20;
